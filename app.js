8c7cda135b97e7081c12a34b3eeb3c1c8e3a9bff
Pablo Novas <pjnovas@gmail.com>
2012-07-09 02:08:57 -0300

    some updates for API version

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
-  app.use(express.session({ secret: 'your secret here' }));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
@@ -32,10 +31,14 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
-app.get('/api/calendario/actual', routes.getAño);
-app.get('/api/calendario/:year', routes.getAño);
+app.get('/api/v1/actual', routes.getAño);
+app.get('/api/v1/:year', routes.getAño);
 
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log("NoLaborables server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
 
-app.listen(3000, function(){
-  console.log("Feriados server listening on port %d in %s mode", app.address().port, app.settings.env);
+process.on("uncaughtException", function (err) { 
+  console.log('>>>>>> Unhandled Exception Ocurred: ' + err);
 });
